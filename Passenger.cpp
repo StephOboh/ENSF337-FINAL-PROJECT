aa075a6607e24c7e666003cbca18122845024f92
StephO <144174339+StephOboh@users.noreply.github.com>
2023-11-23 16:46:25 -0700

    Update Passenger.cpp

--- Passenger.cpp
+++ Passenger.cpp
@@ -9,20 +9,20 @@ using namespace std;
 #include "Passenger.hpp"
 #include <iostream>
 
-Passenger::Passenger() : id(0), seat(nullptr) {}
+Passenger::Passenger() : id(0) {}
 
-Passenger::Passenger(const std::string& firstName, const std::string& lastName, const std::string& phoneNumber, int id, Seat* seat)
+Passenger::Passenger(const std::string& firstName, const std::string& lastName, const std::string& phoneNumber, int id, Seat seat)
     : firstName(firstName), lastName(lastName), phoneNumber(phoneNumber), id(id), seat(seat) {}
 
 void Passenger::displayInfo() const {
-    std::cout << firstName << " " << lastName << " " << phoneNumber << " " << seat->get_row() << seat->get_seat() << " " << id << std::endl;
+    std::cout << firstName << " " << lastName << " " << phoneNumber << " " << seat.get_row() << seat.get_seat() << " " << id << std::endl;
 }
 
 int Passenger::get_ID() const {
     return id;
 }
 
-Seat* Passenger::get_seat() const {
+Seat Passenger::get_seat() const {
     return seat;
 }
 
@@ -37,3 +37,4 @@ std::string Passenger::get_lastName() const {
 std::string Passenger::get_phoneNumber() const {
     return phoneNumber;
 }
+
