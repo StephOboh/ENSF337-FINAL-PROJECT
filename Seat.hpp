0b23ef5391fb0fdfd5e3f3646b4cef2a854727cf
StephO <144174339+StephOboh@users.noreply.github.com>
2023-11-23 16:38:06 -0700

    Create Seat.hpp

---
+++ Seat.hpp
@@ -0,0 +1,24 @@
+//  Seat.hpp
+#include <stdio.h>
+
+#ifndef Seat_hpp
+#define Seat_hpp
+
+class Seat {
+public:
+    Seat();
+    Seat(int row, char seat);
+    void reserve();
+    void release();
+    bool isReserved() const;
+    int get_row() const;
+    char get_seat() const;
+
+private:
+    int row;
+    char seat;
+    bool reserved;
+};
+
+
+#endif /* Seat_hpp */
